5da37429a4d3363e0d53bc76d88ce9e422485c9d
tanuj <your_email@example.com>
2025-11-11 22:17:18 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import Home from "./pages/Home"
 
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-      <Home/>
+
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+
+      </BrowserRouter>
+      
     </>
 
   )
