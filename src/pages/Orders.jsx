806878df6c7c2939f8444316f480e573fdaa04db
tanuj <you@3example3.com>
2026-02-16 18:02:14 +0530

    d

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -13,22 +13,22 @@ export default function Orders() {
 
                             <div className="order_info_cover">
                                 <div className="order_info_box">
-                                    <div className="top_boxf">
-                                        <h5>Order Id: ORD-sghghrfg</h5>
-                                        <h5>Name: Tanuj Saini</h5>
-                                        <h5>Date: 12-02-2026 | 11:04:26 pm</h5>
-                                        <h5>Order Type: Delivery</h5>
+                                    <div className="top_box">
+                                        <h5><span>Order Id</span> ORD-sghghrfg</h5>
+                                        <h5><span>Date</span> 12-02-2026 | 11:04:26 pm</h5>
+                                        <h5><span>Order</span> Type: Delivery</h5>
+                                        <h5><span>Total</span> $399</h5>
                                     </div>
                                     <ul className="order_item_list">
-                                        <li>Name: Burger <span>Price: $10</span></li>
-                                        <li>Name: Pasta <fspan>Price: $20</span></li>
-                                        <li>Name: Pizza <span>Price: $25</span></li>
-                                        <li>Name: Mecaroni <span>Price: $11</span></li>
-                                        <li>Name: Sandwich <span>Price: $30</span></li>
+                                        <li>Burger <span>$10</span></li>
+                                        <li>Pasta <span>$20</span></li>
+                                        <li>Pizza <span>$25</span></li>
+                                        <li>Mecaroni <span>$11</span></li>
+                                        <li>Sandwich <span>$30</span></li>
                                     </ul>
-                                    <h5>Toatl: 399</h5>
                                 </div>
-                                
+
+
                             </div>
 
                         </div>
